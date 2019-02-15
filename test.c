4b19a3a29b3c01034a7ec2e3f7ee6f4701e6ca49
samriddha kc <samriddhakc@samriddhas-MacBook-Pro.local>
2019-02-15 04:20:17 -0600

    Latest_Commit

--- test.c
+++ test.c
@@ -3,13 +3,8 @@
 
 int main()
 { 
-   char str_mem[100];
-   char str_1_mem[100];
-   char*str1=str_mem;
-   strcpy(str1,"sam");
-   strcat(str1," ");
-   char*name="kc";
-   strcat(str1,name);
-   printf("%s\n",str1);
-   return 0; 
+char*name[10]={"samriddha","sam"};
+printf("%s",name[0]);
+printf("%s",name[1]);
+return 0; 
 }
--- wish.c
+++ wish.c
@@ -6,9 +6,13 @@
 #include<sys/wait.h>
 #include<string.h>
 
-
+char* strings[100];
+int counter=0;
 void change_working_dir(char*dir_name)
 {
+
+  //For changing directory using to build the cd utility for wish shell
+
 char mem[100]={'\0'};
 char*strng=mem;
 char s[100];
@@ -18,12 +22,44 @@ strcat(strng,str);
 strcat(strng,dir_name);
 strng[strcspn(strng,"\n" )] = '\0';
 chdir(strng);
+
 }
+
+void print_history(char* n)
+{
+
+  if(atoi(n)>counter){
+    printf("Invalid Input!");
+  }
+
+  else{
+
+  for (int i = 0;i<atoi(n);i++) {
+    printf("%s\n",strings[i]);
+  }
+
+  }
+
+
+}
+
+void store_history(char*character)
+{
+
+  strings[counter]=malloc(strlen(character)+1);
+  strcpy(strings[counter],character);
+  counter=counter+1;
+
+}
+
 
 void view_working_dir()
 {
-char s[100]; 
- printf("%s\n", getcwd(s, 100)); 
+
+//For viewing which directory the program belongs to to build the pwd utility for wish shell
+
+char s[100];
+ printf("%s\n", getcwd(s, 100));
 }
 
 int main(int argc,char* argv[]){
@@ -39,11 +75,11 @@ int main(int argc,char* argv[]){
     {
 
      while(strcmp(buffer,"exit\n")!=0 && !feof(stdin))
-     {  
+     {
 
       printf("wish>");
       getline(&buffer,&buffer_size,stdin);
-      
+
       char*varb=strtok(buffer," ");
       char*my_argv[10];
       int i=0;
@@ -52,35 +88,48 @@ int main(int argc,char* argv[]){
        my_argv[i]=varb;
        varb=strtok(NULL," ");
        i=i+1;
-      } 
+      }
       my_argv[i+1]=NULL;
       if (strcmp(my_argv[0],"cd")==0){
-        change_working_dir(my_argv[1]); 
-      } 
+        change_working_dir(my_argv[1]);
+        store_history("cd");
+      }
       else if (strcmp(my_argv[0],"pwd\n")==0){
         view_working_dir();
-      } 
+      }
+      else if (strcmp(my_argv[0],"history")==0){
+        store_history("history");
+        if (my_argv[1]!=NULL)
+        {
+        print_history(my_argv[1]);
+       }
+      }
       else if (strcmp(my_argv[0],"ls\n")==0){
+         store_history("ls");
          int rc=fork();
          if (rc==0)
          {
            char*my_argv_1[10];
            my_argv_1[0]="/bin/ls";
            my_argv_1[2]=NULL;
-           int exec_rc=execv("/bin/ls",my_argv_1);
+           int exec_rc=execv("/bin/ls",my_argv_1); //completes replaces the code base by a different process
          }
          else
          {
          int wait_rc=wait(NULL);
-         } 
+         }
       }
 
     }
     free(buffer);
-    } 
+    }
 
-   else{ 
+   else{
     /*read file from batch_script*/
+   }
+
+   for (int i = 0;i<counter;i++) {
+       free(strings[i]);
    }
 
    return 0;
